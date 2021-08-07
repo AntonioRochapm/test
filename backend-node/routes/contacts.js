0374ab6d604bd17952313f75df82e86271ee2b56
antonioMorim <antonio.rocha.t0118430@edu.atec.pt>
2021-08-07 17:00:28 +0100

    First

---
+++ backend-node/routes/contacts.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import { findContactById } from '../middlewares/contact.js'
+import { getAllContacts,getContact,updateContact,deleteContact,createContact } from '../controllers/contact.js'
+
+const router = express.Router()
+
+//Get all
+console.log(getAllContacts);
+router.get('/', getAllContacts)
+
+//Get one
+router.get('/:id', findContactById, getContact)
+
+//Create one
+router.post('/',createContact)
+
+//Update one
+router.put('/:id',findContactById, updateContact )
+
+//Delete one
+router.delete('/:id', findContactById, deleteContact)
+
+
+export {router}
